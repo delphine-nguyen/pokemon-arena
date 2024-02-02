cae3e5320e410784c34da5d6506ad08f6ec6f019
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2024-02-02 11:16:46 +0100

    (chore) Update alt of img when hovering

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,10 +1,11 @@
-let goPokedexImg = document.querySelector('#goPokedex\\" > img');
+const goPokedexImg = document.querySelector('#goPokedex\\" > img');
 
 goPokedexImg.addEventListener("mouseover", (event) => {
 	goPokedexImg.src = "./assets/img/pokedex_open.png";
+	goPokedexImg.alt = "Open pokedex";
 });
 
 goPokedexImg.addEventListener("mouseleave", (event) => {
 	goPokedexImg.src = "./assets/img/pokedex_closed.png";
-	goPokedexImg.alt = "Open pokedex";
+	goPokedexImg.alt = "Closed pokedex";
 });
