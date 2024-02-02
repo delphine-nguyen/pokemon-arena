b0cec760754b6a603c03fe504b27de51e9c1ca68
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2024-02-02 10:29:18 +0100

    (refactor+style) Move form to create pokedex in standalone file + basic style for choice menu

--- index.php
+++ index.php
@@ -4,52 +4,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/theme.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
     <title>Pokemon Arena</title>
 </head>
 
-<body>
-    <header>
-        <h1>Pokemon Arena</h1>
+<body class="secondaryColorBg">
+
+    <header class="primaryColorBg">
+        <h1 class="fontMain">Pokemon Arena</h1>
     </header>
 
     <main>
-        <form action="./createPokemon.php" method="post">
-            <article>
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name" value="Bulbizarre">
-            </article>
-            <article>
-                <label for="hp">HP</label>
-                <input type="number" name="hp" id="hp" value="10" min="1">
-            </article>
-            <article>
-                <label for="atk">Attack</label>
-                <input type="number" name="atk" id="atk" value="2" min="1">
-            </article>
-            <article>
-                <label for=" sprite">Sprite</label>
-                <input type="url" name="sprite" id="sprite" value="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png">
-            </article>
-            <article>
-                <label for="typePokemon">Type</label>
-                <select name="typePokemon" id="typePokemon">
-                    <option value="normal">Normal</option>
-                    <option value="fire">Fire</option>
-                    <option value="water">Water</option>
-                    <option value="plant">Plant</option>
-                </select>
-            </article>
-            <button type="submit">Submit</button>
-        </form>
-        <section class="logError">
-            <?php
-            session_start();
-            if (isset($_SESSION["errorMsg"]) && !empty($_SESSION["errorMsg"])) {
-                echo "<p class='errorMsg'>" . $_SESSION["errorMsg"] . "</p>";
-            }
-            ?>
+
+        <section id="menu">
+            <a href="./atelier.php" id="create">
+                <img src="./assets/img/pokeball_closed.png" alt="Closed pokeball">
+            </a>
+
+            <a href=" ./pokedex.php" id=goPokedex">
+                <img src="./assets/img/pokedex_closed.png" alt="Closed pokedex">
+            </a>
         </section>
+
     </main>
+
+    <script type="text/javascript" src="./assets/js/animations.js"></script>
 </body>
 
 </html>
