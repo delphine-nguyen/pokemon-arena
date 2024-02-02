ad27318019e85c898655ba083a3922fb814af85f
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2024-02-02 12:55:21 +0100

    (chore) Separate images of pokemon from header to prep for parallax

--- index.php
+++ index.php
@@ -12,9 +12,24 @@
 
 <body class="secondaryColorBg">
 
+    <section class="parallax">
+        <img src="./assets/img/ponyta.png" alt="Ponyta">
+        <img src="./assets/img/chansey.png" alt="Chansey">
+        <img src="./assets/img/tangela.png" alt="Tanglea">
+        <img src="./assets/img/magmar.png" alt="Magmar">
+        <img src="./assets/img/seaking.png" alt="Seaking">
+        <img src="./assets/img/lapras.png" alt="Lapras">
+        <div class="blankSpace"> </div>
+        <img src="./assets/img/dewgong.png" alt="Dewgong">
+        <img src="./assets/img/ivysaur.png" alt="Ivysaur">
+        <img src="./assets/img/clefable.png" alt="Cleafable">
+        <img src="./assets/img/nidoran.png" alt="Nidoran">
+        <img src="./assets/img/krabby.png" alt="Krabby">
+        <img src="./assets/img/poliwrath.png" alt="Poliwrath">
+    </section>
+
     <header class="primaryColorBg">
         <h1 class="fontMain">Pokemon Arena</h1>
-        <img src="./assets/img/ivysaur.png" alt="Ivysaur">
     </header>
 
     <main>
