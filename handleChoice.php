61b5db60fd483938c6bc115a1c43c5228b53571f
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2024-02-05 09:01:28 +0100

    (feat) Go straight to main when redirected to page Pokedex

--- handleChoice.php
+++ handleChoice.php
@@ -14,11 +14,11 @@ if (isset($_GET['fight'])) {
 
     if (count($chosen) - 1 > 2) {
         $_SESSION["errorMsg"] = "Only choose two contestants at a time";
-        header("Location:./pokedex.php");
+        header("Location:./pokedex.php#main");
         exit();
     } elseif (count($chosen) - 1 < 2) {
         $_SESSION["errorMsg"] = "Please select two pokemons";
-        header("Location:./pokedex.php");
+        header("Location:./pokedex.php#main");
         exit();
     } else {
 
@@ -40,10 +40,10 @@ if (isset($_GET['fight'])) {
         $pokemon->setHp($pokemon->getMaxHp());
     }
     $_SESSION["errorMsg"] = "";
-    header("Location: ./pokedex.php");
+    header("Location: ./pokedex.php#main");
     exit();
 } elseif (isset($_GET['delete'])) {
     session_destroy();
-    header("Location: ./index.php");
+    header("Location: ./index.php#main");
     exit();
 }
