ead20becfcf22f0cb3ce9de9de7e65d9ef163c2b
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2024-02-05 12:38:58 +0100

    (chore) Add link to JS

--- index.php
+++ index.php
@@ -37,6 +37,7 @@
     </main>
 
     <script type="text/javascript" src="./assets/js/animations.js"></script>
+    <script type="text/javascript" src="./assets/js/background.js"></script>
 </body>
 
 </html>
